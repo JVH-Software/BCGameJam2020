67d2bbbef80cae00cd80bee5f04e74868ad01955
John Ordoyo <gtom_1984@hotmail.ca>
2020-02-15 14:47:51 -0800

    refactor as of now

--- Assets/Scripts/ControlNode.cs
+++ Assets/Scripts/ControlNode.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class ControlNode
 {
+    private const double PERCENT_OF_CAPTURED = 1f;
     public enum Team {Wolves, Bears, Chickens, NoTeam}
     public enum State {Captured, Capturing, Empty, Contested}
 
+    private double percentageOfCapture;
     private Team owner;
     private Team capturingTeam;
     private State status;
@@ -15,6 +17,7 @@ public class ControlNode
 
     public ControlNode()
     {
+        percentageOfCapture = 0;
         owner = Team.NoTeam;
         capturingTeam = Team.NoTeam;
         status = State.Empty;
@@ -22,12 +25,12 @@ public class ControlNode
     }
 
     // Setters and getters for team
-    public Team getTeam()
+    public Team getOwner()
     {
         return owner;
     }
 
-    public void setTeam(Team team)
+    public void setOwner(Team team)
     {
         owner = team;
     }
@@ -42,17 +45,22 @@ public class ControlNode
     {
         status = state;
     }
-   /*private void setState(State state, Team team)
+
+   private void setState(State state, Team team)
     {
         switch (state)
         {
+            //sets status to captured with no one else trying to capture
             case State.Captured:
                 owner = team;
-                capturingTeam = Team.NoTeam;
+                capturingTeam = Team.NoTeam ;
                 break;
+            //sets status to capturing with capturing team being the passed in team
             case State.Capturing:
                 capturingTeam = team;
+                percentageOfCapture = 0;
                 break;
+            //adds team to challengers
             case State.Contested:
                 capturingTeam = Team.NoTeam;
                 challengers.Add(team);
@@ -64,33 +72,58 @@ public class ControlNode
     }
     public void setStateEmpty()
     {
+        //special case to empty a control node when no one captures it
+        percentageOfCapture = 0;
+        owner = Team.NoTeam;
         status = State.Empty;
         capturingTeam = Team.NoTeam;
         challengers.Clear();
     }
-    public void Enter(Team challenger)
-    {
-        // if node is uncontested, let the entering team take the node, regardless of
-        if (getState() != State.Contested)
-        {
-            setState(State.Capturing, challenger);
-        }
 
-        // if a challenging team comes in, let the owner and the challenger contest
-        else if (challenger != owner && !challengers.Contains(challenger))
-        {
-            setState(State.Contested, challenger);
-        }
+    public bool isEmptyState()
+    {
+        return (percentageOfCapture == 0 && owner == Team.NoTeam && status == State.Empty && capturingTeam == Team.NoTeam && challengers.Count == 0);
     }
 
-    public void Capture(Team stayingTeam)
+    public bool isOwnerOccupying()
     {
+        return owner == capturingTeam;
+    }
+    public bool isUncontested()
+    {
+        return !isOwnerOccupying() && isEmptyState();
+    }
+    public void Enter(Team challenger)
+    {
+        if(challenger != owner)
+        {
+            //#1. if node is uncontested, let the entering team take the node, regardless of the owner
+            if (status != State.Contested)
+            {
+                setState(State.Capturing, challenger);
+            }
+            //#2. if a challenging team comes in, let the owner and the challenger contest
+            else if (!challengers.Contains(challenger))
+            {
+                setState(State.Contested, challenger);
+            }
+        }
+        
+    }
+
+    public void Stay(Team stayingTeam)
+    {
+        if(getState() != State.Contested)
+        {
+            setState(State.Capturing, stayingTeam);
+        }
+
 
     }
 
     public void Leave(Team leavingTeam)
     {
-        //assumes if one of the challengers are gone
+        //#1. assumes if one of the challengers are gone
         if (challengers.Contains(leavingTeam))
         {
             challengers.Remove(leavingTeam);
@@ -102,9 +135,11 @@ public class ControlNode
             }
         }
         
-        //if owner has left, let the challengers take over
+        //#2. if owner has left, let the challengers take over
         else if(leavingTeam == owner)
         {
+            //empty the node
+
             //if single challenger, let challenger capture
             if (challengers.Count == 1)
             {
@@ -119,13 +154,28 @@ public class ControlNode
                 }
             }
         }
-
-        //if a team fails to capture with 100%, reset to empty
+        //#3. if a team fails to capture with 100%, reset to empty
         else
         {
             setStateEmpty();
         }
     }
-    */
+
+    public void Capture(double increment)
+    {
+
+        percentageOfCapture += increment;
+        if (percentageOfCapture >= PERCENT_OF_CAPTURED)
+        {
+            setState(State.Captured, capturingTeam);
+        }
+    }
+    public void debugDisplay()
+    {
+        Debug.Log("Current owner: " + owner);
+        Debug.Log("Current capturing team: " + capturingTeam);
+        Debug.Log("Current status: " + status);
+        Debug.Log("Percentage of capture: " + percentageOfCapture);
+    }
 
 }
--- Assets/Scripts/ControlNodeObject.cs
+++ Assets/Scripts/ControlNodeObject.cs
@@ -8,19 +8,15 @@ public class ControlNodeObject : MonoBehaviour
     private const string BEAR_TAG = "Bear";
 
     private double capturePercentage;
-    private int numTeamsOnNode;
 
     private ControlNode node;
     private const double CAPTURE_RATE = 1/3f;
-    private ControlNode.Team occupyingTeam;
 
     SpriteRenderer renderer;
     private void Awake()
     {
         capturePercentage = 0f;
         node = new ControlNode();
-        numTeamsOnNode = 0;
-        occupyingTeam = node.getTeam();
         renderer = GetComponent<SpriteRenderer>();
         StartCoroutine("capture");
         
@@ -48,39 +44,24 @@ public class ControlNodeObject : MonoBehaviour
     //Enter sets contesting state
     private void OnTriggerEnter2D(Collider2D collider)
     {
-
-        //Takes in an object, calls controlNode enter
         string tag = collider.gameObject.tag;
         if (isEntity(tag))
         {
-            numTeamsOnNode++;
-            ControlNode.Team incomingTeam = convertTagToTeam(tag);
-            if(occupyingTeam != incomingTeam && numTeamsOnNode > 1)
-            {
-                node.setState(ControlNode.State.Contested);
-                occupyingTeam = ControlNode.Team.NoTeam;
-            }
-            else if(occupyingTeam != incomingTeam)
-            {
-                occupyingTeam = incomingTeam;
-            }
-
+            ControlNode.Team enteringTeam = convertTagToTeam(tag);
+            node.Enter(enteringTeam);
         }
     }
 
-    //Stay sets capturing state, allows the occupying team(whatever is staying on the collider to capture)
+    
     private void OnTriggerStay2D(Collider2D collider)
     {
         string tag = collider.gameObject.tag;
         if (isEntity(tag))
         {
-            if(numTeamsOnNode == 1)
-            {
-                occupyingTeam = convertTagToTeam(tag);
-                node.setState(ControlNode.State.Capturing);
-            }
+            ControlNode.Team stayingTeam = convertTagToTeam(tag);
+            node.Stay(stayingTeam);
         }
-    }
+    } 
 
     //handles when a previous team leaves and when there is no one on tag
     private void OnTriggerExit2D(Collider2D collision)
@@ -88,29 +69,15 @@ public class ControlNodeObject : MonoBehaviour
         string tag = collision.gameObject.tag;
         if (isEntity(tag))
         {
-            
             ControlNode.Team leavingTeam = convertTagToTeam(tag);
-            Debug.Log("Object that has left: " + leavingTeam);
-            numTeamsOnNode--;
-            if (occupyingTeam == leavingTeam)
-            {
-                occupyingTeam = ControlNode.Team.NoTeam;
-                capturePercentage = 0;
-            }
-            
-            if(numTeamsOnNode == 0 && node.getState() != ControlNode.State.Captured)
-            {
-                node.setState(ControlNode.State.Empty);
-            }
+            node.Leave(leavingTeam);
         }
+
     }
-    /*private void Update()
+    private void Update()
     {
-        /*Debug.Log("Control Point State: " + node.getState());
-        Debug.Log("The Occupying Team: " + occupyingTeam);
-        Debug.Log("Node Captured by: " + node.getTeam());
-        Debug.Log("Number of factions at Node: " + numTeamsOnNode);
-    }*/
+        node.debugDisplay();
+    }
 
     IEnumerator capture()
     { 
@@ -118,44 +85,34 @@ public class ControlNodeObject : MonoBehaviour
         {
             
             ControlNode.State nodeStatus = node.getState();
-
             switch (nodeStatus)
             {
-                case ControlNode.State.Empty:
-                    occupyingTeam = ControlNode.Team.NoTeam;
-                    capturePercentage = 0;
-                    renderer.color = Color.gray;
-                    break;
-
-                case ControlNode.State.Capturing:
-                    if(capturePercentage < 1)
-                    {
-                        capturePercentage += CAPTURE_RATE * Time.deltaTime;
-                        renderer.color = Color.green;
-                    }
-                    else
-                    {
-                        capturePercentage = 1;
-                        node.setState(ControlNode.State.Captured);                        
-                    }
-                    break;
-                case ControlNode.State.Contested:
-                    renderer.color = Color.red;
-                    break;
                 case ControlNode.State.Captured:
-                    node.setTeam(occupyingTeam);
-                    if (node.getTeam() == ControlNode.Team.Bears)
+                    if(node.getOwner() == ControlNode.Team.Bears)
                     {
                         renderer.color = Color.black;
                     }
-                    else if (node.getTeam() == ControlNode.Team.Wolves)
+                    else if(node.getOwner() == ControlNode.Team.Wolves)
                     {
                         renderer.color = Color.blue;
                     }
                     break;
+                case ControlNode.State.Capturing:
+                    double increment = CAPTURE_RATE * Time.deltaTime;
+                    node.Capture(increment);
+                    renderer.color = Color.green;
+                    break;
+                case ControlNode.State.Contested:
+                    renderer.color = Color.red;
+                    break;
+                case ControlNode.State.Empty:
+                    renderer.color = Color.gray;
+                    break;
                 default:
                     break;
             }
+
+            
             yield return null;
         }
         
