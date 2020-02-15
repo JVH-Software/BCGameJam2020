348adeba9fa81a7e5982117009ed3d6bbb2ad0e7
Justin van Heek <justin.vanheek@vhsoftware.dev>
2020-02-14 22:31:37 -0800

    Creates Pack Script with Health & Movement

---
+++ Assets/Scripts/Pack.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pack : MonoBehaviour
+{
+
+    public float health = 10f;
+    public float maxHealth = 10f;
+    public float speed = 1f;
+
+    Rigidbody2D rbody;
+    Animator anim;
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 movement_vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (movement_vector != Vector2.zero)
+        {
+            anim.SetBool("Iswalking", true);
+            anim.SetFloat("Input_x", movement_vector.x);
+            anim.SetFloat("Input_y", movement_vector.y);
+        }
+        else
+        {
+            anim.SetBool("Iswalking", false);
+        }
+
+        rbody.MovePosition(rbody.position + movement_vector * Time.deltaTime);
+    }
+    private void Shoot()
+    {
+        Debug.Log(string.Format("{0} shot its gun.", name));
+    }
+
+    public void Hit(float damage)
+    {
+        ModifyHealth(-damage);
+    }
+
+    private void ModifyHealth(float amount)
+    {
+        health += amount;
+        if(health <= 0)
+        {
+            Death();
+        }
+        else if(health > maxHealth)
+        {
+            health = maxHealth;
+        }
+    }
+
+    private void Death()
+    {
+        Debug.Log(string.Format("{0} died.", name));
+    }
+}
