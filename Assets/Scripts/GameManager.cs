4a8ec28713be47c46fc4449e3f57741a757f50bf
John Ordoyo <gtom_1984@hotmail.ca>
2020-02-14 22:30:35 -0800

    Created Control Node and GameManager class

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+  
+    public const int NUM_NODES = 5;
+
+    //control Nodes
+    // - all the characters
+    // - returns the current controlling team
+
+    List<string> characters = new List<string>();
+    List<string> nodes = new List<string>();
+    //control Nodes 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
