8743e54f41b4cdd7b9fe626519059a57d28e1e85
Justin van Heek <justin.vanheek@vhsoftware.dev>
2020-02-14 22:41:18 -0800

    Creates PlayerPack subclass with movement

---
+++ Assets/Scripts/PlayerPack.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPack : Pack
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        movementVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+}
